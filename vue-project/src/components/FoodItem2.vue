53abb36170516da8ec354a872bab3c00628f2f1d
Birccy <rpboado3@student.apc.edu.ph>
2025-02-24 14:28:26 +0800

    added a second component

--- vue-project/src/components/FoodItem2.vue
+++ vue-project/src/components/FoodItem2.vue
@@ -10,8 +10,8 @@
 export default {
     data() {
         return {
-            name: 'Apples',
-            message: 'I like apples',
+            name: 'Mangoes',
+            message: 'I like mangoes',
             clicks: 0
         }
     },
@@ -29,6 +29,6 @@ export default {
     display: inline-block;
     margin: 10px;
     padding: 10px;
-    background-color: lightgreen;
+    background-color: lightyellow;
   }
 </style> 
