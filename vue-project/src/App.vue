23c7ffae9c795dceaf820ad07d6f2b1c9f11dfce
Birccy <rpboado3@student.apc.edu.ph>
2025-03-02 15:13:04 +0800

    fixed database and etc. ig

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,32 +1,15 @@
-<script setup>
-import { ref, onMounted } from 'vue'
-import { supabase } from './lib/supabaseClient'
-
-const countries = ref([])
-
-async function getCountries() {
-    const { data } = await supabase.from('countries').select()
-    countries.value = data
-}
-
-onMounted(() => {
-    getCountries()
-})
-</script>
-
 <template>
-    <ul>
-        <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
-    </ul>
   <h1>Food</h1>
   <personal-profile/> <br>
+  <country/>
   <food-item/>
   <food-item2/>
   <food-item/>
   <food-item2/>
+  <comment-form/>
+  <comment/>
+   
+
 </template>
 
-
-
-
-<style></style>
+<script></script>
