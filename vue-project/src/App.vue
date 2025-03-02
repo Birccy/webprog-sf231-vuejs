fcdfd1bfd8cb413c729d18eb9240822bb5e55210
Birccy <rpboado3@student.apc.edu.ph>
2025-03-02 15:16:17 +0800

    fixed a bit of ordering

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <h1>Food</h1>
+  <food-item/>
+  <food-item2/>
+  <food-item/>
+  <food-item2/>
   <personal-profile/> <br>
   <country/>
-  <food-item/>
-  <food-item2/>
-  <food-item/>
-  <food-item2/>
   <comment-form/>
   <comment/>
    
